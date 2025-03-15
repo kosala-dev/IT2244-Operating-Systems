8f0cb5d472c45bd0efa84c992bf7f1713194dab5
Kosala Madhushan <69857444+kosala-dev@users.noreply.github.com>
2025-03-15 18:24:29 +0530

    Add files via upload

---
+++ IT2244-Day 01/Test01.bat
@@ -0,0 +1,37 @@
+mkdir Critieria_1
+cd Critieria_1
+mkdir Standar_1
+mkdir Standar_2 
+mkdir Standar_3
+mkdir Standar_4 
+mkdir Standar_5
+cd..
+mkdir Critieria_2
+cd Critieria_2
+mkdir Standar_1
+mkdir Standar_2
+mkdir Standar_3
+cd..
+mkdir Critieria_3
+cd Critieria_3
+mkdir Standar_1
+mkdir Standar_2
+mkdir Standar_3
+mkdir Standar_4
+mkdir Standar_5
+cd..
+mkdir Critieria_4
+cd Critieria_4
+mkdir Standar_1
+mkdir Standar_2
+mkdir Standar_3
+mkdir Standar_4
+cd..
+mkdir Critieria_5
+cd Critieria_5
+mkdir Standar_1
+mkdir Standar_2
+cd..
+mkdir Critieria_6
+cd Critieria_6
+mkdir Standar_1
